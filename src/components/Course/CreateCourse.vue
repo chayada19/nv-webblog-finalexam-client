c7a8305886db7eccece4ec5d3171474262197860
chayada19 <112927775+chayada19@users.noreply.github.com>
2023-10-18 14:29:05 +0700

    final-exam

--- src/components/Course/CreateCourse.vue
+++ src/components/Course/CreateCourse.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-      <h1>create 1 course</h1>
+  <div>
+    <h1>create 1 course</h1>
+    <div v-on:submit.prevent="createUser">
+          <div>title: {{ i.title }}</div>
+          <div>instructor: {{ i.instructor }}</div>
+          <div>description: {{ i.description }}</div>
+          <div>total_student: {{ i.total_student.length }}</div>
+          <div>status: {{ i.status }}</div>
+          <div>type: {{ i.type }}</div>
+
+          <div><button v-on:click="navigateTo('/course/' + i.id)">all courses</button></div>
+          <div><button @click="navigateTo('/course/edit/' + i.id)">edit course</button></div>
+          <div><button @click="deleteCourse(i)">delete course</button></div>
+          <div><button v-on:click="logout">Logout</button></div>
+          <hr>
+      </div>
     </div>
-       
+</template>
+<script>
+import UsersService from "@/services/UserService";
+
+export default {
+  data() {
+    return {
+      user: {
+        name: "",
+        lastname: "",
+        email: "",
+        password: "",
+        status: "active"
+      }
+    };
+  },
+  methods: {
+    async createUser() {
+      try {
+        await UsersService.post(this.user);
+        this.$router.push({
+          name: "users"
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
+</script>
+<style scoped></style>
+ 
